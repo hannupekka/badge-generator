29fcec4e566379c3dd7b6f7ec6daad8228712620
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2017-01-11 15:07:29 +0200

    Beautify code and add color pickers for names.

--- js/docs.js
+++ js/docs.js
@@ -1,31 +1,31 @@
-$(function() {
+$(function () {
     // Code for docs demos
     function createColorpickers() {
         // Api demo
-        
 
-        $('.demo-auto').colorpicker().on('changeColor.colorpicker', function(event){
+
+        $('.demo-auto').colorpicker().on('changeColor.colorpicker', function (event) {
             var part = $(this).data("part"),
                 color = event.color.toHex();
 
-            if(part == "bgHeader" || part == "bgFooter"){
-                $("."+part).css("background-color", color);
-            }else{
-                $("."+part).css("color", color);
+            if (part == "bgHeader" || part == "bgFooter") {
+                $("." + part).css("background-color", color);
+            } else {
+                $("." + part).css("color", color);
             }
-            console.log("Var "+ $(this).data("part") +": ",event.color.toHex());
+            console.log("Var " + $(this).data("part") + ": ", event.color.toHex());
         });
     }
     createColorpickers();
-    $("#logoSrc").change(function(){
-        $(".logo").attr("src",$(this).val());
+    $("#logoSrc").change(function () {
+        $(".logo").attr("src", $(this).val());
         console.log($(this).val());
     });
-    $("#pdf").click(function(){
+    $("#pdf").click(function () {
         var params = $("form").serialize().replace(/%23/g, "");
         console.log(params);
-        
-        $(this).attr("href","bagde-generator.html?"+params);
+
+        $(this).attr("href", "bagde-generator.html?" + params);
     });
 
 
